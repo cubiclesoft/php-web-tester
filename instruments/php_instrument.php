7ca1c77722abc2022da75203a2b056544207457f
cubiclesoft <webmaster@cubiclesoft.com>
2022-02-26 16:52:46 -0700

    Reset PHP opcode cache.

--- instruments/php_instrument.php
+++ instruments/php_instrument.php
@@ -1,10 +1,12 @@
 <?php
 	// Sample PHP instrumentation file for Web Test.
-	// (C) 2018 CubicleSoft.  All Rights Reserved.
+	// (C) 2022 CubicleSoft.  All Rights Reserved.
 
 	error_reporting(E_ALL);
 	ini_set("display_startup_errors", true);
 	ini_set("display_errors", "1");
 	ini_set("log_errors", true);
 	ini_set("error_log", str_replace("\\", "/", dirname(__FILE__)) . "/instrumented_php.log");
+
+	if (function_exists("opcache_reset"))  @opcache_reset();
 ?>
